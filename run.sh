e064b401e5366078e696e556fefeefaeb1983282
Rikke Uldbæk <75247443+rikkeuldbaek@users.noreply.github.com>
2023-05-30 08:40:19 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
+#packages
+python3 -m pip install -r requirements.txt
+
 # run the scripts
 python3 src/emotion_classification_FAKE.py
 python3 src/emotion_classification_REAL.py
 python3 src/emotion_classification_ALL.py
-
-
